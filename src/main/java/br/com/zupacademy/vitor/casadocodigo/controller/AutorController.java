bf56f84bd24c740854ed69d2ad297dfe22a280c1
Vitor Rodrigues <vitorodriguesantos@hotmail.com>
2021-05-18 15:26:05 -0300

    Validando Email

--- src/main/java/br/com/zupacademy/vitor/casadocodigo/controller/AutorController.java
+++ src/main/java/br/com/zupacademy/vitor/casadocodigo/controller/AutorController.java
@@ -1,5 +1,7 @@
 package br.com.zupacademy.vitor.casadocodigo.controller;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+import org.springframework.web.method.annotation.ExceptionHandlerMethodResolver;
 
 import br.com.zupacademy.vitor.casadocodigo.dto.FormAutor;
 import br.com.zupacademy.vitor.casadocodigo.modelo.Autor;
@@ -23,6 +27,10 @@ public class AutorController {
 	@PostMapping
 	@Transactional
 	public String novoAutor(@RequestBody @Valid FormAutor form) {
+		Optional<Autor> verificaEmail = autorRepository.findByEmail(form.getEmail());
+		if(verificaEmail.isPresent()) {
+			return ("O e-mail "+form.getEmail()+" ja possui cadastro no sistema.");
+		}
 		Autor autor = new Autor(form.getNome(), form.getEmail(), form.getDescricao());
 		autorRepository.save(autor);
 		return autor.toString();
--- src/main/java/br/com/zupacademy/vitor/casadocodigo/repository/AutorRepository.java
+++ src/main/java/br/com/zupacademy/vitor/casadocodigo/repository/AutorRepository.java
@@ -1,10 +1,14 @@
 package br.com.zupacademy.vitor.casadocodigo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.zupacademy.vitor.casadocodigo.modelo.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, Long>{
+
+	Optional<Autor> findByEmail(String email);
 	
 
 }
