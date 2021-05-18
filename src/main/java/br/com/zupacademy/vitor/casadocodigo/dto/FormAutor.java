e66bc48f26025226f95c77adbbbb8e2082f8fef2
Vitor Rodrigues <vitorodriguesantos@hotmail.com>
2021-05-18 13:43:39 -0300

    Novo Autor

---
+++ src/main/java/br/com/zupacademy/vitor/casadocodigo/dto/FormAutor.java
@@ -0,0 +1,26 @@
+package br.com.zupacademy.vitor.casadocodigo.dto;
+
+public class FormAutor {
+	
+	private String nome;
+	private String email;
+	private String descricao;
+	
+	public FormAutor() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+}
